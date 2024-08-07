d7e0fdbd962d9323c6cdd71884e7eab1e013320a
Ryan Tan <ryantan.mail@gmail.com>
2024-08-07 00:20:19 -0700

    new keymap, unused language mason pruned

--- lua/ryan/core/keymaps.lua
+++ lua/ryan/core/keymaps.lua
@@ -2,4 +2,5 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", {desc = "Clear search highlights"})
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "gb", ":e#<CR>", { desc = "Return to last visited page" })
